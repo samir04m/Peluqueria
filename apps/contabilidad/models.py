7745d04ccf4798cd5caa87ecba93677432b936b3
Samir Mejia <samir04m@gmail.com>
2018-09-08 18:22:44 -0500

    Creacion de modelos y migraciones

--- apps/contabilidad/models.py
+++ apps/contabilidad/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Ingreso(models.Model):
+    dueno = models.DecimalField(max_digits=10, decimal_places=2)
+    empleados = models.DecimalField(max_digits=10, decimal_places=2)
+    insumos = models.DecimalField(max_digits=10, decimal_places=2)
--- apps/servicio/models.py
+++ apps/servicio/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Empleado(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=70)
+
+    def __str__(self):
+        return "{}  ({})".format(self.nombre, self.apellidos)
+
+class Servicio(models.Model):
+    nombre = models.CharField(max_length=50)
+
+class Tipo(models.Model):
+    nombre = models.CharField(max_length=50)
+    servicio = models.ForeignKey(Servicio, null=False, blank=False, on_delete=models.CASCADE)
+
+class Subtipo(models.Model):
+    nombre = models.CharField(max_length=50)
+    precio = models.DecimalField(max_digits=7, decimal_places=2)
+    tipo = models.ForeignKey(Tipo, null=False, blank=False, on_delete=models.CASCADE)
+
+class Factura(models.Model):
+    fecha = models.DateTimeField(default=timezone.now)
+    empleado = models.ForeignKey(Empleado, null=False, blank=False, on_delete=models.CASCADE)
+    servicios = models.ManyToManyField(Servicio)
