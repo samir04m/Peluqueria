a465bc5939960e8388a047c80102b37b2caecf3a
Samir Mejia <samir04m@gmail.com>
2018-09-08 20:49:56 -0500

    Se agregaron los archivos Css y Js

---
+++ static/js/myScript.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('select').material_select();
+    $(".button-collapse").sideNav();
+
+    $('.dropdown-button').dropdown({
+         belowOrigin: true,
+         constrainWidth: false
+    });
+
+
+});
