dc0476208c501930ebfd65682695df77d7e068be
Samir Mejia <samir04m@gmail.com>
2018-09-12 01:18:46 -0500

    Se implemento slider en pagina de inicio

--- static/js/myScript.js
+++ static/js/myScript.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+    $('.slider').slider({
+        height: 500
+    });
+    
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+    });
+
+
     $('select').material_select();
     $(".button-collapse").sideNav();
 
